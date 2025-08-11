a445596908c61fd8d50a49ec6a8b16832a45b36c
Sahithyareddy17 <kondapallysahithyareddy@gmail.com>
2025-08-11 21:42:03 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,10 @@
 from flask import Flask
 app=Flask(__name__)
-@app.route("/")
-def home():
-  return "Hello from Flask in Docker!"
+@app.get("/health")
+def health():
+  return jsonify(status="ok")
+@app.get("/books")
+def books():
+  return jsonify(data=["Clean Code", "Programatic Programmer"])
 if__name__=="__main__":
   app.run(host="0.0.0.0",port=5000)
